57b4f6391439b264dd8d05359072bf146b53acd3
CMNrep <148292840+CMNrep@users.noreply.github.com>
2024-09-20 09:37:56 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
 import Evento from "./models/evento.js"
 
-const evento = new Evento("test-gravacao", "2024/12/11", "12:12", "PP", 10.00, 10, "nada nada nada, aqui nao tem nada")
+const evento = new Evento("test-gravacao2", "2024/12/11", "12:12", "PP", 10.00, 10, "nada nada nada, aqui nao tem nada")
 
-// evento.incluir().then(() => {
+// await evento.incluir().then(() => {
 //     console.log("evento cadastrado com sucesso")
 
 // }).catch((err) =>{
 //     console.log(err)
 // })
 
-// evento.excluir("test1").then(()=>{
-//     console.log("evento excluido")
-// }).catch((err) =>{
-//     console.log(err)
-// })
+// const evento1 = new Evento("test-gravacao", "2024/12/11", "12:12", "PP", 10.00, 10, "aqui nada tem")
 
-// const evento1 = new Evento("test1", "2024/12/11", "12:12", "PP", 10.00, 10, "aqui nada tem")
-
-// evento.alterar(evento1).then(() => {
+// await evento.alterar(evento1).then(() => {
 //     console.log("alterado com sucesso")
 // }).catch((err) =>{
 //     console.log(err)
 // })
 
+// await evento.excluir("test-gravacao2").then(()=>{
+//     console.log("evento excluido")
+// }).catch((err) =>{
+//     console.log(err)
+// })
 
-evento.consultar("").then((listaEventos) => {
+evento.consultar("hdaiwu").then((listaEventos) => {
     for(const evento of listaEventos) {
         console.log(evento.toString())
     }
